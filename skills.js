935e842757d851f26e2624d98227622be697941d
Mahesh Rathod <rathod.mahesh@vistora.co.in>
2025-05-03 04:21:25 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,10 +1,3 @@
-function calculteSkillLevel(skill) {
-  const skillLevels = {
-    beginner: 1,
-    intermediate: 2,
-    advanced: 3,
-    expert: 4,
-  };
-
-  return skillLevels[skill] || 0;
+function calculateNumbers(num1, num2) {
+    return num1 + num2;
 }
