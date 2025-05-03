feb51ecb62dbbf41011a557f62ebc336f82dc889
Mahesh Rathod <rathod.mahesh@vistora.co.in>
2025-05-03 04:35:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,10 @@
-//create web server
+//create web server with express
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Comment = require('./models/Comment');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
 const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -14,11 +14,3 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-//middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//routes
